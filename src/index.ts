2e6c2b287e75656b3ac25e00cdfb46980ecadb97
Luis <luis.e@ges.inatel.br>
2024-08-22 22:50:49 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+  let greatestNumber: number = Math.max(...candies);
+  let response: Array<boolean> = []
+
+  candies.map(x => {
+    x += extraCandies;
+    
+    if (x >= greatestNumber) {
+      return response.push(true);
+    };
+
+    return response.push(false);
+    });
+    return response;
+};
+
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
